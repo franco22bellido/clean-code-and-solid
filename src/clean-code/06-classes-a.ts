47b672c05242295bbf9deb98f770ceac5c19407b
franco22bellido <franco.bellido22@gmail.com>
2024-07-22 20:48:58 -0300

    introducción a clases

---
+++ src/clean-code/06-classes-a.ts
@@ -0,0 +1,17 @@
+(()=> {
+    type Gender = 'M' | 'F'
+    class Person {
+        public name: string;
+        public gender: Gender;
+        public birthdate: Date;
+
+        constructor(name: string, gender: Gender, birthdate: Date){
+            this.name = name;
+            this.gender = gender;
+            this.birthdate = birthdate;
+        }
+    }
+
+    const newPerson = new Person('Fernando', 'M', new Date('1985-10-21'))
+    console.log(newPerson);
+})();
